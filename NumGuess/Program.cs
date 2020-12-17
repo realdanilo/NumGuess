b8498b1af69755fd7b5ccfebf5b0d997af7cdb4b
Danilo <realdanilo@users.noreply.github.com>
2020-12-16 18:53:48 -0700

    Add project files.

---
+++ NumGuess/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int randomNumber = rand.Next(0, 101);
+            int lives = 5;
+            int answer = 0;
+            while(lives > 0)
+            {
+                Console.Write("Guess a number:\n\t");
+                answer = Int32.Parse(Console.ReadLine());
+                if (answer == randomNumber) break;
+                if(answer > randomNumber)
+                {
+                    Console.WriteLine("too high");
+                }
+                else
+                {
+                    Console.WriteLine("too low");
+                }
+                lives--;
+                Console.WriteLine("Wrong, try again\n----\n");
+            }
+            Console.WriteLine($"\n\n\tGuess number was {randomNumber}\n\n\n");
+        }
+    }
+}
